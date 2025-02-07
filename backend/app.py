a9aa2dc6c92eae8cfb13ef185627e2f4dfc602c6
moon4546x <achintya061005@gmail.com>
2025-02-07 17:03:35 +0530

    flask integration

--- backend/app.py
+++ backend/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("home.html")
+    return render_template("homepage.html")
 
 @app.route("/block")
 def block():
@@ -13,10 +13,10 @@ def block():
 
 @app.route("/login")
 def login():
-    return render_template("login.html")
+    return render_template("sign-in.html")
 
 @app.route("/signup")
 def signup():
-    return render_template("signup.html")
+    return render_template("sign-up.html")
 if __name__ == "__main__":
     app.run(debug=True, use_reloader=False)
